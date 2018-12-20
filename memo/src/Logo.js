714c688ae62fb2fbc7d690667f27e5379f76593b
Tan Nguyen <tannd1993@gmail.com>
2018-12-20 11:21:48 +0700

    React.memo

--- memo/src/Logo.js
+++ memo/src/Logo.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-function Logo() {
-  console.log('Logo component');
-  return (
-    <div>This is a logo</div>
-  );
+// function Logo() {
+//   console.log('Logo component');
+//   return (
+//     <div>This is a logo</div>
+//   );
+// }
+
+class Logo extends React.Component {
+  componentDidMount() {
+    console.log('Logo componentDidMount');
+  }
+
+  render() {
+    console.log('Logo component');
+    return (
+      <div>This is a logo</div>
+    );
+  }
 }
 
 export default React.memo(Logo);
